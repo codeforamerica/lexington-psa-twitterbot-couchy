4a0f273c2207d1c1e34d069074954804e4e0d730
Erik Schwartz <erik@erikschwartz.net>
2014-04-11 15:56:30 -0700

    Move bot code to module

---
+++ bot.js
@@ -0,0 +1,71 @@
+var Twit = require('twit');
+
+var _this;
+module.exports = {
+  newTwit: function(twitParams) {
+    _this = this;
+    _this.T = new Twit(twitParams);
+  },
+  streamTerm: function(term, handleTweet) {
+    var stream = _this.T.stream('statuses/filter', {track: term});
+
+    stream.on('tweet', _this.handleTweet);
+    stream.on('error', function(err) {
+      if (err.code === 401) {
+        console.log('Error authenticating with twitter! Please check env variables');
+        process.exit();
+      }
+    });
+  },
+  getStoredTweet: function(twitterHandle, callback) {
+    _this.redis.get(twitterHandle, function(err, tweet) {
+      callback(tweet ? JSON.parse(tweet) : undefined);
+    });
+  },
+  randomTweet: function(callback) {
+    _this.redis.RANDOMKEY(function() {
+      var randomKey = arguments[1];
+      _this.getStoredTweet(randomKey, function(tweet) {
+        callback(tweet);
+      });
+    });
+  },
+  replyTo: function(tweet) {
+    var handle = tweet.user.screen_name;
+    var update = '@' + handle + ' is my new best bud! Here you are saving me from the flaaaaames! http://savecouchy.com/buds/' + handle;
+    _this.T.post('statuses/update', {status: update, in_reply_to_status_id: tweet.id}, function(err, reply) {
+      console.log('err: ' + err);
+      console.log('reply: ' + reply);
+    });
+  },
+  setNewUser: function(tweet) {
+    _this.redis.set(tweet.user.screen_name, JSON.stringify(tweet));
+    if (process.env.NODE_ENV === 'production') {
+      _this.replyTo(tweet);
+    }
+  },
+  handleNewUser: function(tweet) {
+    console.log('new user! ' + tweet.user.screen_name);
+    tweet.embedded = {html: tweet.text};
+    _this.setNewUser(tweet);
+  },
+  handleNewThread: function(tweet) {
+    console.log('omg a new thread!!');
+    _this.redis.get(tweet.user.screen_name, function(err, user) {
+      if (!user) {
+        _this.handleNewUser(tweet)
+      } else {
+        console.log('NOT a new new user ' + tweet.user.screen_name);
+      }
+    });
+  },
+  handleTweet: function(tweet) {
+    _this.latestTweet = tweet;
+    if (! tweet.retweeted_status) {
+      _this.handleNewThread(tweet);
+    } else {
+      console.log('is retweet');
+    }
+    console.log(tweet.user.screen_name);
+  }
+};
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var Twit = require('twit');
+var bot = require('./bot');
+
 var express = require("express");
 var https = require('https');
 var app = express();
@@ -11,103 +12,34 @@ app.use("/public", express.static(__dirname + '/public'));
 
 var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
 
-var T = new Twit({
+bot.newTwit({
     consumer_key:         process.env.API_KEY
   , consumer_secret:      process.env.API_KEY_SECRET
   , access_token:         process.env.ACCESS_TOKEN
   , access_token_secret:  process.env.ACCESS_TOKEN_SECRET
 });
 
-var latestTweet;
-
-var stream = T.stream('statuses/filter', {track: process.env.SEARCH_TERM}, function() {
-  console.log(arguments);
-});
-stream.on('tweet', handleTweet);
-stream.on('error', function(err) {
-  if (err.code === 401) {
-    console.log('Error authenticating with twitter! Please check env variables');
-    process.exit();
-  }
-});
-
-function handleTweet(tweet) {
-  latestTweet = tweet;
-  if (! tweet.retweeted_status) {
-    handleNewThread(tweet);
-  } else {
-    console.log('is retweet');
-  }
-  console.log(tweet.user.screen_name);
-}
-
-function handleNewThread(tweet) {
-  console.log('omg a new thread!!');
-
-  redis.get(tweet.user.screen_name, function(err, user) {
-    if (!user) {
-      handleNewUser(tweet)
-    } else {
-      console.log('NOT a new new user ' + tweet.user.screen_name);
-    }
-  });
-}
-
-function handleNewUser(tweet) {
-  console.log('new user! ' + tweet.user.screen_name);
-  tweet.embedded = {html: tweet.text};
-  setNewUser(tweet);
-}
-
-function setNewUser(tweet) {
-  redis.set(tweet.user.screen_name, JSON.stringify(tweet));
-  if (process.env.NODE_ENV === 'production') {
-    replyTo(tweet);
-  }
-}
-
-function replyTo(tweet) {
-  var handle = tweet.user.screen_name;
-  var update = '@' + handle + ' is my new best bud! Here you are saving me from the flaaaaames! http://savecouchy.com/buds/' + handle;
-  T.post('statuses/update', {status: update, in_reply_to_status_id: tweet.id}, function(err, reply) {
-    console.log('err: ' + err);
-    console.log('reply: ' + reply);
-  });
-}
-
-function randomTweet(callback) {
-  redis.RANDOMKEY(function() {
-    var randomKey = arguments[1];
-    getStoredTweet(randomKey, function(tweet) {
-      callback(tweet);
-    });
-  });
-};
-
-function getStoredTweet(twitterHandle, callback) {
-  redis.get(twitterHandle, function(err, tweet) {
-    callback(tweet ? JSON.parse(tweet) : undefined);
-  });
-}
+bot.redis = redis;
+bot.streamTerm(process.env.SEARCH_TERM, handleTweet);
 
 app.get('/', function(req, res) {
-  if (latestTweet) {
-      res.render('index', { latestTweet: latestTweet });
+  if (bot.latestTweet) {
+      res.render('index', { latestTweet: bot.latestTweet });
   } else {
-    randomTweet(function(tweet) {
+    bot.randomTweet(function(tweet) {
       res.render('index', { latestTweet: tweet });
     });
   }
 });
 
 app.get('/tweet', function(req, res) {
-  randomTweet(function(tweet) {
+  bot.randomTweet(function(tweet) {
     res.send(tweet);
   });
 });
 
 app.get('/buds/:name', function(req, res) {
-  getStoredTweet(req.params.name, function(tweet) {
+  bot.getStoredTweet(req.params.name, function(tweet) {
     if (tweet) {
       res.render('bud', { tweet: tweet, requestUrl: 'http://savecouchy.com/' + req.url })
     } else {
